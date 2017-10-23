02b66e07746b1a8023ced02cc0d99250ae634208
rsulkowski <Ryszard.Sulkowski@gmail.com>
2017-10-23 23:44:06 +0200

    [FEATURE] Added inheritance support, examples and unit tests to check new feature. Change of the previus text property for javadocs of the Builder methods to value to remove boilerplate code of annotation.

---
+++ app/src/main/java/eu/rsulkowski/jdoocsoup/app/Fruit.java
@@ -0,0 +1,8 @@
+package eu.rsulkowski.jdoocsoup.app;
+
+/**
+ * Created by rizzz on 2017-10-23.
+ */
+
+class Fruit {
+}
---
+++ app/src/test/java/eu/rsulkowski/jdoocsoup/FruitAndAppleBuilderTest.java
@@ -0,0 +1,8 @@
+package eu.rsulkowski.jdoocsoup;
+
+/**
+ * Created by rizzz on 2017-10-23.
+ */
+
+public class FruitAndAppleBuilderTest {
+}
